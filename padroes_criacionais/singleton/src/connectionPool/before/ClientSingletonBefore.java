74492383cc9e3a3bc8d6dc77add50b7be9b059b4
SladeSouzaSantos <greencity.sustentavel@gmail.com>
2023-11-05 21:35:22 -0300

    projeto de referencia para aplicar o padrão.

---
+++ padroes_criacionais/singleton/src/connectionPool/before/ClientSingletonBefore.java
@@ -0,0 +1,35 @@
+package connectionPool.before;
+
+import connectionPool.before.conn.Connection;
+import connectionPool.before.conn.ConnectionPool;
+
+public class ClientSingletonBefore {
+	public static void doQuery1() {
+		ConnectionPool pool = new ConnectionPool();
+		Connection conn = pool.getConnection();
+		if(conn != null)
+			conn.query("SELECT * FROM A1");
+	}
+	
+	public static void doQuery2() {
+		ConnectionPool pool = new ConnectionPool();
+		Connection conn = pool.getConnection();
+		if(conn != null)
+			conn.query("SELECT * FROM A2");
+	}
+	
+	public static void doQuery3() {
+		ConnectionPool pool = new ConnectionPool();
+		Connection conn = pool.getConnection();
+		if(conn != null)
+			conn.query("SELECT * FROM A3");
+	}
+
+	public static void main(String[] args) {
+		doQuery1();
+		doQuery2();
+		doQuery3();
+		
+		//3 connections was made, even with 2 connections limit
+	}
+}
