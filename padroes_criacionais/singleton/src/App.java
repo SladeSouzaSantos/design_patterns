f8637900e56aa29a5f4051f1ce41b6f97e22b056
SladeSouzaSantos <greencity.sustentavel@gmail.com>
2023-11-07 00:23:36 -0300

    Alteração na ordem de execução dos exemplos.

--- padroes_criacionais/singleton/src/App.java
+++ padroes_criacionais/singleton/src/App.java
@@ -1,9 +1,19 @@
+import connectionPool.after.ClientSingletonFechandoConexao;
+import connectionPool.after.ClientSingletonLimitConexaoError;
 import connectionPool.before.ClientSingletonBefore;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("BEFORE");
+        System.out.println("\n\nBEFORE");
         System.out.println("--------------");
         ClientSingletonBefore.exemplo();
+
+        System.out.println("\n\nSINGLETON (Sem erro de limite de conexão, por fecha-las após o uso.)");
+        System.out.println("--------------");
+        ClientSingletonFechandoConexao.exemplo();
+
+        System.out.println("\n\nSINGLETON (Com erro de limite de conexão, por não fecha-las após o uso.)");
+        System.out.println("--------------");
+        ClientSingletonLimitConexaoError.exemplo();
     }
 }
