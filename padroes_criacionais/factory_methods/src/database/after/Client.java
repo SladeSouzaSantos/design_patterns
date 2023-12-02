a2eec8aeacf235c3da799efe48bf667d9a4b7d07
SladeSouzaSantos <greencity.sustentavel@gmail.com>
2023-12-02 11:36:32 -0300

    Refatoração da classe client do exemplo db.

--- padroes_criacionais/factory_methods/src/database/after/Client.java
+++ padroes_criacionais/factory_methods/src/database/after/Client.java
@@ -1,13 +1,16 @@
 package database.after;
 
 import database.after.db.DB;
+import database.after.factory.DBFactory;
 import database.after.factory.OracleFactory;
 
 public class Client {
 
 	public static void main(String[] args) {
-//		DB db = new PostgresFactory().getDatabase();
-		DB db = new OracleFactory().getDatabase();
+		DBFactory dbFactory = new OracleFactory();
+
+		DB db = dbFactory.getDatabase();
+		
 		db.query("SELECT * FROM users");
 		db.update("INSERT INTO users VALUES ('User', 25)");
 	}
