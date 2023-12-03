2c84400e15d8899a51d8f9095dc56a9b6624d6a0
SladeSouzaSantos <greencity.sustentavel@gmail.com>
2023-12-03 13:56:59 -0300

    Implementação do padrão Prototype e variações.

---
+++ padroes_criacionais/prototype/src/App.java
@@ -0,0 +1,8 @@
+import animation.Client;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Prototype Animation");
+        Client.main(args);
+    }
+}
